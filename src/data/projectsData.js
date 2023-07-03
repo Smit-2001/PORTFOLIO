d3495d0c62e4ea3ea87705fcadf2b2fd65329898
Smit Shah <100117550+Smit-2001@users.noreply.github.com>
2023-07-03 18:10:24 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -10,7 +10,6 @@ export const projectsData = [
       'Book store is an online web application where customer can purchase books online. Through a web browser the customers can search for a book by its title or author, later can add to the shopping cart and finally purchase the books.',
     tags: ['React', 'CSS', 'Material Ui'],
     code: 'https://github.com/Smit-2001/Book-Store',
-    demo: 'https://book-store-qe87.onrender.com/',
     image: book,
   },
   {
@@ -19,7 +18,6 @@ export const projectsData = [
     projectDesc: 'Eshop website is a web application where customer can purchase products. Here, user can register, login, search for products, apply filters, add products to cart, checkout and place the order..',
     tags: ['React', 'Express', 'Node JS', 'MongoDB', 'PayPal'],
     code: 'https://github.com/Smit-2001/Eshop',
-    demo: 'https://github.com/Smit-2001/Eshop',
     image: eshop,
   },
   {
@@ -29,7 +27,6 @@ export const projectsData = [
       'This project about recommending the movies based on the similarity of movie attributes.',
     tags: ['Python', 'Machine Learning', 'Flask'],
     code: 'https://github.com/Smit-2001/Movie-Recommendation-System',
-    demo: 'https://github.com/Smit-2001/Movie-Recommendation-System/blob/master/README.md',
     image: movie,
   },
 ];
