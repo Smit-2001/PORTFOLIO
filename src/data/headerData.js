5b9dc0eb71180e984030e44abfb87710a78d13c5
Smit Shah <100117550+Smit-2001@users.noreply.github.com>
2023-07-06 23:44:00 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import Resume from "../../src/assets/Resume_Smit Shah.pdf";
 export const headerData = {
     name: 'SMIT SHAH',
     title: "Hello! I am",
-    desciption: "Hello! I am SMIT SHAH, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and i am Passionate about Problem Solving. ",
+    desciption: "Hello! I am SMIT SHAH, a passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and i am Passionate about Problem Solving. ",
     image: Smit,
     resumePdf: Resume
 }
