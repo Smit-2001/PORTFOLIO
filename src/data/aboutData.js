5e69977a4cabe8b682601dc5d9cb3e728218d3e3
Smit Shah <100117550+Smit-2001@users.noreply.github.com>
2023-07-06 23:40:29 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,6 @@
 export const aboutData = {
-    title: "Who am I",
-    description1: "My name is Smit Shah. I'm a MERN developer.",
-    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and I am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, ReactJs, Bootstrap, ExpressJs, MongoDB, Mysql, Firebase, etc.",
-}
+  title: 'Who am I',
+  description1: "My name is Smit Shah. I'm a MERN Stack Developer.",
+  description2:
+    'I am a passionate programmer. A quick learner with a self-learning attitude. I love to learn and explore new technologies and I am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Node.js, React.js, Express.js, MongoDB, Angular JS, Bootstrap, MySQL , Firebase, etc.',
+};
