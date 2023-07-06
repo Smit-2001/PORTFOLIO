4e5f8f68c594999259caf236e0fb0244f78bb9dc
Smit Shah <100117550+Smit-2001@users.noreply.github.com>
2023-07-06 23:41:45 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,16 +1,9 @@
 export const experienceData = [
-    {
-        id: 1,
-        company: 'Tatvasoft',
-        jobtitle: 'React Developer Intern',
-        startYear: 'May 2023',
-        endYear: 'June 2023'
-    },
-    {
-        id: 2,
-        company: 'Suven Consultants and Technology Pvt. Ltd.',
-        jobtitle: 'Machine Learning Intern',
-        startYear: 'April 2023',
-        endYear: 'May 2023'
-    },
-]
+  {
+    id: 1,
+    company: 'Tatvasoft',
+    jobtitle: 'React Developer Intern',
+    startYear: 'May 2023',
+    endYear: 'June 2023',
+  },
+];
